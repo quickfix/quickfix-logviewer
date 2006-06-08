f1107cec49dc4de47327cac32989b699972b87f1
Oren Miller <oren@quickfixengine.org>
2006-06-08 08:33:48 +0000

    Allocate additional memory to VM

--- lib/quickfix-logviewer.sh
+++ lib/quickfix-logviewer.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-$JAVA_HOME/bin/java -classpath quickfixj.jar:quickfix-logviewer.jar quickfix.logviewer.Main FIX44.xml
+$JAVA_HOME/bin/java -Xmx512M -classpath quickfixj.jar:quickfix-logviewer.jar quickfix.logviewer.Main FIX44.xml
