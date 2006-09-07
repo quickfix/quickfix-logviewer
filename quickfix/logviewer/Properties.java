3fc287f36f6c7e5c1945f535c7442d43f0e5a598
Oren Miller <oren@quickfixengine.org>
2006-09-07 21:19:20 +0000

    Updated version information

--- quickfix/logviewer/Properties.java
+++ quickfix/logviewer/Properties.java
@@ -22,7 +22,7 @@ package quickfix.logviewer;
 public class Properties extends java.util.Properties {
 	
 	final static int MAJOR_VERSION = 1;
-	final static int MINOR_VERSION = 0;
+	final static int MINOR_VERSION = 1;
 	final static int REVISION = 0;
 	
 	static private Properties instance = null;
